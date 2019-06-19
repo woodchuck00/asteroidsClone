75b86df00dff0cb07cf7049b32fafdc6cdd9c6a2
Theo Theodor <theo@onlyWire.com>
2019-06-19 14:10:14 -0400

    initial game

---
+++ Assets/Scripts/EuclideanTorus.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EuclideanTorus : MonoBehaviour {
+
+    // Update is called once per frame
+    void Update() {
+        // Teleport the game object
+        if (transform.position.x > 9) {
+            transform.position = new Vector3(-9, transform.position.y, 0);
+        }
+        else if (transform.position.x < -9) {
+            transform.position = new Vector3(9, transform.position.y, 0);
+        }
+        else if (transform.position.y > 6) {
+            transform.position = new Vector3(transform.position.x, -6, 0);
+        }
+        else if (transform.position.y < -6) {
+            transform.position = new Vector3(transform.position.x, 6, 0);
+        }
+    }
+}
